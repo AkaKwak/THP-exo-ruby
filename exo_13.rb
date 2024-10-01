d1e6a691ed6178a61d020802d98e9eb36e38a8d4
AkaKwak <christophe.alaterre@gmail.com>
2024-10-01 19:27:04 +0200

    exo 13

---
+++ exo_13.rb
@@ -0,0 +1,12 @@
+#exo_13.rb
+
+# Écris un programme exo_13.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
+# 
+  5.times do |tenth|
+    # puts "jean.dupont.#{tenth}@email.fr"
+    10.times do |unit|
+        puts "jean.dupont.#{tenth}#{unit}@email.fr"
+  # puts "jean.dupont.#{tenth}#{unit}@email.fr"
+    end
+  end
+
