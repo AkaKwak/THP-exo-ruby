da9e67c38bd03d6c29d7145ed997b106ea6ac154
AkaKwak <christophe.alaterre@gmail.com>
2024-10-01 17:18:36 +0200

    Mes 8 premiers exercices

--- exo_08.rb
+++ exo_08.rb
@@ -1,7 +1,5 @@
 #exo_08.rb
 
-inverted_count_end = 0
-
 puts "Entrez un nombre :"
 print ">"
 number = gets.chomp.to_i
@@ -9,7 +7,3 @@ number = gets.chomp.to_i
 number.times do |i|
   puts number - i
 end
-
-
- # puts number.times do |new_number|
-#puts new_number - number
