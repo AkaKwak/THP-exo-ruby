652d959bb93b0dca5f615adce4277a00fd5131f8
AkaKwak <christophe.alaterre@gmail.com>
2024-10-01 21:11:02 +0200

    exo 14

--- exo_13.rb
+++ exo_13.rb
@@ -1,7 +1,18 @@
 #exo_13.rb
-
+  array_email = []
   5.times do |tenth|
     10.times do |unit|
-        puts "jean.dupont.#{tenth}#{unit}@email.fr"
+        email = "jean.dupont.#{tenth}#{unit}@email.fr"
+          puts email
+        array_email.push(email)
     end
   end
+
+
+  # Écris un programme exo_13.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails : "jean.dupont.01@email.fr"
+
+  # Création de l'array pour stocker les emails
+# emails = []
+
+# Boucle pour générer 50 emails
+# 50.times do |i|
