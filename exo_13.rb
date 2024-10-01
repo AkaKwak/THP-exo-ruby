4d8198964e46893f3bfa2f6013399e5e6663bd74
AkaKwak <christophe.alaterre@gmail.com>
2024-10-01 20:15:19 +0200

    exo 14 fdp

--- exo_13.rb
+++ exo_13.rb
@@ -1,9 +1,7 @@
 #exo_13.rb
 
   5.times do |tenth|
-    # puts "jean.dupont.#{tenth}@email.fr"
     10.times do |unit|
         puts "jean.dupont.#{tenth}#{unit}@email.fr"
-  # puts "jean.dupont.#{tenth}#{unit}@email.fr"
     end
   end
---
+++ exo_14.rb
@@ -0,0 +1,10 @@
+#exo_14.rb tu m'auras fait suer !
+
+5.times do |tenth|
+  10.times do |unit|
+    # PIPE pour faire les comparaisons OR / == c'est un test "comparaison"
+    if unit == 0 || unit == 2 || unit == 4 || unit == 6 || unit == 8
+      puts "jean.dupont.#{tenth}#{unit}@email.fr"
+    end
+  end
+end
